269d54c067cd38a90bfcaa29276b1435a8c299b8
nickianimeforever24 <niseekah.hamilton@gmail.com>
2015-12-03 22:11:08 +0000

    Grading App

--- gradingapp.js
+++ gradingapp.js
@@ -6,5 +6,18 @@ function giveGrade() {
   alert("hey!");
 
 //Input your code below:
+if (grade<=89 && grade >=80) {
+  alert("B");
+} 
+if (grade<100 && grade>=90) {
+  alert("A");
+}
+if (grade<79 && grade>=70) {
+  alert("C");
+} else if (grade>65 && grade<69) {
+  alert("D");
+} else if (grade<65) {
+  alert("F")
+}
 
   }
